830232cdf948bd3ae971d985216c7c55664a50b5
duarteviih098-prog <duarteviih098@gmail.com>
2026-06-05 09:06:40 -0300

    Update factory.py

--- app/factory.py
+++ app/factory.py
@@ -16,6 +16,9 @@ def create_app(config_class=Config):
     application.config.from_object(config_class)
     application.secret_key = application.config['SECRET_KEY']
 
+    from app.config import validate_production_config
+    validate_production_config(application.config['SECRET_KEY'])
+
     application.wsgi_app = ProxyFix(
         application.wsgi_app,
         x_for=1,
